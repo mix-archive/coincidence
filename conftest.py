5cfaa3dc10a496c40bf7546f0b54faef46cc8ca8
Mix <32300164+mnixry@users.noreply.github.com>
2024-12-28 03:02:25 +0800

    feat(profiler): add command-line option to enable profiling for tests

--- conftest.py
+++ conftest.py
@@ -6,14 +6,7 @@ from pyinstrument import Profiler
 PROFILE_ROOT = Path(__file__).parent / ".profiles"
 PROFILE_ROOT.mkdir(exist_ok=True, parents=True)
 
-
-@pytest.fixture(autouse=True, scope="module")
-def auto_profile(request: pytest.FixtureRequest):
-    # Turn profiling on
-    with Profiler() as profiler:
-        yield  # Run test
-    results_file = PROFILE_ROOT / f"{request.module.__name__}.html"  # pyright:ignore[reportUnknownMemberType]
-    profiler.write_html(results_file)
+FIXTURE_FILE = Path(__file__).parent / "tests" / "fixtures.tar.lzma"
 
 
 def pytest_addoption(parser: pytest.Parser):
@@ -23,3 +16,20 @@ def pytest_addoption(parser: pytest.Parser):
         default=10000,
         help="The height of the block to utxo sync",
     )
+    parser.addoption(
+        "--enable-profiler",
+        action="store_true",
+        help="Enable the profiler for all tests",
+    )
+
+
+@pytest.fixture(autouse=True, scope="module")
+def auto_profile(request: pytest.FixtureRequest, pytestconfig: pytest.Config):
+    if not pytestconfig.getoption("--enable-profiler"):
+        yield
+        return
+    # Turn profiling on
+    with Profiler() as profiler:
+        yield  # Run test
+    results_file = PROFILE_ROOT / f"{request.module.__name__}.html"  # pyright:ignore[reportUnknownMemberType]
+    profiler.write_html(results_file)
